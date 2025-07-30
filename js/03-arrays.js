58b83ba2ff7a44d2168e8ed5dcd716e816352f8a
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-07-30 17:30:06 -0300

    Update 03-arrays.js

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log('teste');
+// para saber se tudo ta certo, a conexão console.log('teste');
+
+
+// Declarando um array (vetor)
+let vocalistas = ["Ozy", "Dio", "Lemmy", "David Gilmour"];
+console.log(vocalistas);
+// console.log(vocalista); para visualizar a estrutura do array inteiro
+
+
+console.log(vocalistas[0]);
+//accesando um determinando elemento através do índice
+
+// O david Gilmour está em turnê tocando Pink Floyd
+
+console.log(`O ${vocalistas[3]} esta em turnê tocando Pink Floyd`);
+
+
 
