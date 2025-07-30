ec3737bbc6345898a1b699a73ad0ee2bd97a76e7
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-07-30 16:51:45 -0300

    exercicio de teste

---
+++ js/02-meu-script.js
@@ -0,0 +1,36 @@
+'use strict';
+//Habilitando o modo estrito
+
+
+/* Exercícios */
+
+/* Crie uma variável chamada "uc" e coloque nela o número da UC que estamos*/
+
+// 2) Crie uma variável chamada "Competencia e coloque nela uma frase que representa o que estamos estudandos  nesta UC"
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando FRONT-END com JavaScript.
+
+
+let uc = 2;
+
+let competencia = "html, css, e JavaScript";
+
+
+// modelo 1
+console.log("Eu sou Elioise, e eu estou estudando  no Uc"+uc+" eu estou aprendendo "+competencia+" ");
+
+//Modelo 2
+console.log(`Eu tou estudando UC ${uc} e Eu estou aprendendo ${competencia}` );
+
+//Correto! Aspas duplas como comando, e simples como texto
+
+console.log('gosto "muito" de paçoca!');
+
+
+
+
+
+
+
+
+
