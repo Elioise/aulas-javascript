69111d69edf9ab2ab9c1bf95cf74b7cd3bb3ba8e
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-08-06 16:52:21 -0300

    exemplos de objetos 2

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -1,6 +1,8 @@
 'use stict';
 
 // Exemplo 1: objeto com dados de uma pessoa
+console.log("exemplo 1\n");
+
 const pessoa = {
     nome: "chaves", 
     idade: 8,
@@ -13,5 +15,50 @@ console.log(pessoa);
 //Acessamos as propriedades usando nomeDoObjeto.nomeDaPropriedade
 
 console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
+
+//exemplo 2 : objeto com array
+console.log("\nExemplo2\n");
+
+const livro = {
+    titulo: "O Senhos dos Anéis",
+    autor: "J.R.R Tolkien",
+    volumes: [
+        "A sociedade do Anel",
+        "O retorno do Rei",
+
+    ]
+};
+
+console.log(livro);
+
+console.log(livro.titulo);
+
+/* Para accesar algum dos volumes, primeiro passamos pela propriedade (volumes) usando 'ponto' e, chegando nela, usamos os 'colchetes' com índice pois se trata de um array */
+
+console.log(`Meu livro preferido é ${livro.volumes[1]}`);
+
+
+const cor = {
+    maca: "vermelho",
+    limao: "verde",
+    farinha: "branco"
+};
+
+console.log(cor.limão);
+console.log(`Precisa do limão ${cor.limao} para fazer suco de limão`);
+
+console.log("A cor de maçã é "+cor.maca+"" );
+
+
+
+
+
+
+
+
+
+
+
+
 
 
