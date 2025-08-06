7d916132644937877d8e3514083417d248c57f83
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-08-06 16:31:19 -0300

    usando objeto

---
+++ js/04-objetos.js
@@ -0,0 +1,17 @@
+'use stict';
+
+// Exemplo 1: objeto com dados de uma pessoa
+const pessoa = {
+    nome: "chaves", 
+    idade: 8,
+    cidade: "são paulo",
+    estado: "SP"
+}; 
+
+console.log(pessoa);
+
+//Acessamos as propriedades usando nomeDoObjeto.nomeDaPropriedade
+
+console.log(`O ${pessoa.nome} tem ${pessoa.idade} anos.`);
+
+
