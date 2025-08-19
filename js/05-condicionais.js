72a5a345306d70c7c4390348844ddbe355812070
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-08-19 14:46:28 -0300

    aula 5 condicionais

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -15,4 +15,19 @@ console.log("Se vc está vendo essa mensagem é porque a condiçao é VERDADEIRA
 
     }
 
+    console.log("\nExemplo 2");
+    let aluno = "OZZY";
+    let idade = 50;
+
+    /*Logíca : verificar se o aluno é maior ou menor idade */
+    if( idade < 18 ){
+       console.log("Voce é menor de idade!");  
+    }
+
+    else{
+        console.log("voce é maior de idade!");
+        
+    }
+   
+    
     
