1c6187340ec374ed65e76aee6c27ebcb1c30d85d
Elioise <elioise.rmprevost@senacsp.edu.br>
2025-08-27 14:02:50 -0300

    queryselector

--- js/09-dom-eventos.js
+++ js/09-dom-eventos.js
@@ -2,7 +2,8 @@
 
 /*Exemplo 01 */
 //procurando (querySeletor) por um elemento chamado exemplo1
-const exemplo1 =document.querySelector("#exemplo1");
+const exemplo1 =document.querySelector //só para id
+("#exemplo1");
 const mensagem01 =document.querySelector("#mensagem01");
 const pagina = document.querySelector("body");
 
@@ -10,5 +11,11 @@ const pagina = document.querySelector("body");
 console.log(exemplo1);
 console.log(mensagem01);
 console.log(pagina);
+
+/*
+Para selecionar vários elementos, use querySelectorAll. O resultado é uma esp´[ecie de array de elementos (NodeList).
+*/
+const paragrafos = document.querySelectorAll("p")
+console.log(paragrafos);
 
 
